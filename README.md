1c154becaa79d2f991b01a1f744b1c7f847f784e
Zachary Kramer <zckramer83@gmail.com>
2022-07-22 14:14:58 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+this template was made with the help of the blog at: 
+https://blog.appsignal.com/2022/01/19/how-to-set-up-a-nodejs-project-with-typescript.html
+
+7/22/2022
+Zachary Kramer
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+function sayMyName(name: string): void {
+    if (name === "Heisenberg") {
+        console.log("You're right!")
+    } else {
+        console.log("You're wrong...")
+    }
+}
+
+sayMyName("Heisenberg");
